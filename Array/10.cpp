61bb8213586b78fdb38948a63e1f6dcb0b0d31c1
Hard-Coder05 <smishra1605@gmail.com>
2021-04-18 17:39:12 +0530

    DSA final 450

--- Array/10.cpp
+++ Array/10.cpp
@@ -1,3 +1,56 @@
+//SUCCESSFUL SOLUTION
+//
+//
+//
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution
+{
+public:
+    int minJumps(int arr[], int n)
+    {
+        int curri = n - 1;
+        int count = 0;
+        while (curri != 0)
+        {
+            for (int i = 0; i < curri; i++)
+            {
+                if (arr[i] >= curri - i)
+                {
+                    curri = i;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i, j;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        Solution obj;
+        cout << obj.minJumps(arr, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
 //TLE SOLUTION
 //
 //
