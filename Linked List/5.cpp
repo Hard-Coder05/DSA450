b07e9d06c79c2c316ea519730312903db2d20bef
Hard-Coder05 <smishra1605@gmail.com>
2021-06-09 23:01:19 +0530

    dsa450

---
+++ Linked List/5.cpp
@@ -0,0 +1,26 @@
+Node *detectFirstNode(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    Node *slow = head, *fast = head;
+    slow = slow->next;
+    fast = fast->next->next;
+    while (fast && fast->next)
+    {
+        if (slow == fast)
+            break;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (slow != fast)
+        return NULL;
+    slow = head;
+    while (slow != fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
+}
